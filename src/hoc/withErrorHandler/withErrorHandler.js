b9a50045e795390fded39bcb5d8f7258554cfe05
JaeWoogie <60237088+JaeWoogie@users.noreply.github.com>
2020-09-13 21:11:44 -0400

    burger app with routing

---
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -0,0 +1,49 @@
+import React, { Component } from "react";
+
+import Modal from "../../components/UI/Modal/Modal";
+import Aux from "../../hoc/Aux";
+
+const withErrorHandler = (WrapperComponent, axios) => {
+  return class extends Component {
+    state = {
+      error: null,
+    };
+
+    componentWillMount() {
+      this.reqInterceptor = axios.interceptors.request.use((req) => {
+        this.setState({ error: null });
+        return req;
+      });
+
+      this.resInterceptor = axios.interceptors.response.use(
+        (res) => res,
+        (error) => {
+          this.setState({ error: error });
+        }
+      );
+    }
+
+    componentWillUnmount() {
+      console.log("willUnmount", this.reqInterceptor, this.resInterceptor);
+      axios.interceptors.request.eject(this.reqInterceptor);
+      axios.interceptors.response.eject(this.resInterceptor);
+    }
+
+    errorConfirmedHandler() {
+      this.setState({ error: null });
+    }
+
+    render() {
+      return (
+        <Aux>
+          <Modal show={this.state.error} clicked={this.errorConfirmedHandler}>
+            {this.state.error ? this.state.error.message : null}
+          </Modal>
+          <WrapperComponent {...this.props} />;
+        </Aux>
+      );
+    }
+  };
+};
+
+export default withErrorHandler;
